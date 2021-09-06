33b79053db6436be7e3fb2c92e03df1669aa8d00
carlos <carlos@carlos.localhost>
2021-09-06 10:13:09 -0600

    funcion 5) retornar carnets agregada/no funciona

--- main.c
+++ main.c
@@ -1,18 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-
 #include "unitTest.h"
 
-#define MAXCHAR 60
-
-typedef struct 
+/*typedef struct 
 {
     int array_csv[59][6];
     int array_csvT[6][59];
     int array_csvM[59][59];
-}arrays2d;
+}arrays2d;*/
 
 void leerA_archivo(char *direccion_archivo, arrays2d *array_principal)
 {
@@ -27,28 +20,34 @@ void leerA_archivo(char *direccion_archivo, arrays2d *array_principal)
 
     x = 0;
 
-    while(feof(archivo_csv) != true)
+    while (feof(archivo_csv) != true)
     {
 
         y = 0;
 
         fgets(linea_archivo, MAXCHAR, archivo_csv);
-        
+
         token = strtok(linea_archivo, ",");
-        while(token != NULL)
+        while (token != NULL)
         {
             int tamanio_token = strlen(token);
 
-            if(tamanio_token == 1 || tamanio_token == 2)
+            if (tamanio_token == 1 || tamanio_token == 2)
             {
                 int valor_token = atoi(token);
-                array_principal->array_csv[(x - 2)][y] = valor_token;
+                array_principal->array_csvN[(x - 2)][y] = valor_token;
 
                 y++;
             }
-            
+            else
+            {
+                array_principal->array_csv[x][0] = token;
+                //printf("%s", array_principal->array_csv[x][0]);
+            }
+
             token = strtok(NULL, ",");
         }
+        //printf("\n");
         x++;
     }
     fclose(archivo_csv);
@@ -57,11 +56,11 @@ void leerA_archivo(char *direccion_archivo, arrays2d *array_principal)
 void trasponer_array(arrays2d *array_principal)
 {
     int x, y;
-    for(x = 0; x < 6; x++)
+    for (x = 0; x < 6; x++)
     {
-        for(y = 0; y < 59; y++)
+        for (y = 0; y < 59; y++)
         {
-            array_principal->array_csvT[x][y] = array_principal->array_csv[y][x];
+            array_principal->array_csvT[x][y] = array_principal->array_csvN[y][x];
         }
     }
 }
@@ -71,26 +70,32 @@ void multiplicar_array(arrays2d *array_principal)
 
     int x, y, z;
 
-    for(x = 0; x < 59; x++)
+    for (x = 0; x < 59; x++)
     {
-        for(y = 0; y < 59; y++)
+        for (y = 0; y < 59; y++)
         {
             array_principal->array_csvM[x][y] = 0;
         }
     }
 
-    for(x = 0; x < 59; x++)
+    for (x = 0; x < 59; x++)
     {
-        for(y = 0; y < 59; y++)
+        for (y = 0; y < 59; y++)
         {
-            for(z = 0; z < 6; z++)
+            for (z = 0; z < 6; z++)
             {
-                array_principal->array_csvM[x][y] += ((array_principal->array_csv[x][z]) * (array_principal->array_csvT[z][y]));
+                array_principal->array_csvM[x][y] += ((array_principal->array_csvN[x][z]) * (array_principal->array_csvT[z][y]));
             }
         }
     }
 
     //return *array_principal;
+}
+
+const char *integrantes()
+{
+
+    return "hs20006,gr20035";
 }
 
 int main(void)
@@ -104,30 +109,5 @@ int main(void)
     trasponer_array(&array_principal);
     multiplicar_array(&array_principal);
 
-    /*for(x = 0; x < 59; x++)
-    {
-        for(y = 0; y < 6; y++)
-        {
-            printf("%d", array_principal.array_csv[x][y]);
-        }
-        printf("\n");
-    }
-
-    for(x = 0; x < 6; x++)
-    {
-        for(y = 0; y < 59; y++)
-        {
-            printf("%d", array_principal.array_csvT[x][y]);
-        }
-        printf("\n");
-    }*/
-
-    for(x = 0; x < 59; x++)
-    {
-        for(y = 0; y < 59; y++)
-        {
-            printf("%d ", array_principal.array_csvM[x][y]);
-        }
-        printf("\n");
-    }
+    printf("\n%s\n", integrantes());
 }
