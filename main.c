729cb0c5b919c4a2c0be51ad0ac396070aa2ecb3
Carlos Gomez <gr20035@ues.edu.sv>
2021-10-07 17:01:19 -0600

    implementacion de funciones de pilas y colas

--- main.c
+++ main.c
@@ -1,37 +1,75 @@
 #include "pila.h"
+#include "cola.h"
 
-int main(void){
-    
+int main(void) {
+
     PILA* pila = crear_pila();
-    int x = 1, n, data;
-    while(x){
-        printf("Opciones\n1 - Agregar a pila\n2 - Extraer de pila\n3 - Obtner primer elemento\n4 - Esta vacia?\n");
-        scanf("%d", &n);
-        switch(n){
+    COLA* cola = crear_cola();
+
+    int x, n, data;
+
+    do {
+        printf("1 - Pila\n2 - Cola\n");
+        scanf("%d", &x);
+        switch (x) {
             case 1:
-                printf("Elemento:\n");
-                scanf("%d", &data);
-                insertar_en_pila(pila, data);
+                do {
+                    printf("Opciones\n1 - Agregar a pila\n2 - Extraer de pila\n3 - Obtner primer elemento\n4 - Esta vacia?\n");
+                    scanf("%d", &n);
+                    switch (n) {
+                        case 1:
+                            printf("Elemento:\n");
+                            scanf("%d", &data);
+                            insertar_en_pila(pila, data);
+                            break;
+                        case 2:
+                            extraer_de_pila(pila);
+                            break;
+                        case 3:
+                            printf("Primer elemento: %f\n", get_primeroPila(pila));
+                            break;
+                        case 4:
+                            if (pila_vacia(pila)) {
+                                printf("NO!\n");
+                            } else {
+                                printf("SI!\n");
+                            }
+                            break;
+                    }
+                } while (n > 0 && n < 5);
                 break;
             case 2:
-                extraer_de_pila(pila);
+                do {
+                    printf("Opciones\n1 - Agregar a cola\n2 - Extraer de cola\n3 - Obtner primer elemento\n4 - Esta vacia?\n");
+                    scanf("%d", &n);
+                    switch (n) {
+                        case 1:
+                            printf("Elemento:\n");
+                            scanf("%d", &data);
+                            insertar_en_cola(cola, data);
+                            break;
+                        case 2:
+                            extraer_de_cola(cola);
+                            break;
+                        case 3:
+                            printf("Primer elemento: %d\n", get_primeroCola(cola));
+                            break;
+                        case 4:
+                            if (cola_vacia(cola)) {
+                                printf("NO!\n");
+                            } else {
+                                printf("SI!\n");
+                            }
+                            break;
+                    }
+                } while (n > 0 && n < 5);
                 break;
-            case 3:
-                printf("Primer elemento: %f\n", get_primeroPila(pila));
-                break;
-            case 4:
-                if(pila_vacia(pila)){
-                    printf("SI!\n");
-                }else{
-                    printf("NO!\n");
-                }
-                break;
-            default:
-                x = 0;
+
         }
-    }
-    
+    } while (x == 1 || x == 2);
+
+    eliminar_cola(cola);
     eliminar_pila(pila);
-    
+
     return 0;
 }
